a782865ddfa3695fe1138576b6f2483c590efa83
Deon <110722148+DanielEmmanuel1@users.noreply.github.com>
2025-01-16 11:48:52 +0100

    Fixes #2 -Build-Landing-Page-#2 (#20)
    * [FRONTEND] Build Landing Page #2
    
    * chore: clean ups

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
-import './App.css'
+import './index.css'
+import Navbar from './components/Nav'
+import Heropage from './components/Hero'
+import Metrics from './components/Metrics'
+import Content from './components/Content'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
-      <div>
-        <h1>WinSaved</h1>
+      <div className="bg-[#030B1E] h-screen bg-hero-semi-circle">
+        <Navbar />
+        <Heropage />
       </div>
+      <Metrics />
+
+      <div className="bg-[#030B1E]">
+        <Content />
+      </div>
+      <Footer />
     </>
   )
 }
